d6d75fd27d9d61cb26a6b42db5823e1de260f10f
Ingo Becker <ingo@orgizm.net>
2013-11-14 19:46:56 +0100

    fixed sign up link

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -5,5 +5,6 @@ feature 'The Homepage' do
     visit root_path
     page.should have_content('Login into your crate')
     page.should have_content('Create your own crate')
+    page.should have_link('Sign up', href: new_user_registration_path)
   end
 end
