ec083c640909f0f7290c8b29d317daa62f0d4bc0
Ingo Becker <ingo@orgizm.net>
2013-11-14 19:11:36 +0100

    added seeds for artist/album/track models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,16 @@
 User.create! email: 'ingo@orgizm.net', password: 'foobarbaz', password_confirmation: 'foobarbaz'
+
+artists = []
+10.times do 
+  artists << Artist.create!(name: Faker::Name.last_name)
+end
+
+artists.each do |artist|
+  albums = []
+  rand(1..10).times do 
+    albums << artist.albums.create!(name: Faker::Lorem.word)
+    rand(8..14).times do 
+      albums.last.tracks.create!(name: Faker::Lorem.word, duration: 420)
+    end
+  end
+end
